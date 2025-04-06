bbf3bf822ea86ddc98714a1e7214e17034deec86
Katherine Wang <kwang18@br013.ib.bridges2.psc.edu>
2025-04-06 17:49:42 -0400

    Update submit_hal.sh to take CLIs

--- submit_hal.sh
+++ submit_hal.sh
@@ -10,64 +10,153 @@
 #SBATCH -A bio230007p
 ##SBATCH --gres=gpu:1
 
+
 # hal and HALPER; installation documentation says you have to set these manually, without ~
-# I think that adding these lines was key to getting halLiftover to run, because the job no longer finishes within seconds
+# IMPORTANT: if you are running this on your own device, change the paths to match your own paths to the hal and halLiftover repositories
+# These lines are key to getting halLiftover to run.
 export PATH=/jet/home/kwang18/repos/hal/bin:${PATH}
 export PYTHONPATH=/jet/home/kwang18/repos/halLiftover-postprocessing:${PYTHONPATH}
 
 module load anaconda3
 source activate hal
-# source activate base
-#conda activate hal
 
 # It's important to allow 12 hours for halLiftover because it does take a long time
 # To run all halLiftover jobs in parallel, just submit multiple jobs. 
-cd ~ # ensure that the output directory ends up in the home directory; when running things on a cluster, this is important
 
 # give execute permissions to scripts
 find ~/repos/halLiftover-postprocessing/ -type f -exec chmod a+x {} \;
 
-outdir="output/hal/Mouse/Pancreas"
-# outdir="output/hal/Mouse/Ovary"
-# outdir="output/hal/Human/Pancreas"
-# outdir="output/hal/Human/Ovary" # take this as CLI or parse this from CLI later
-mkdir -p $outdir # use -p flag so there's no error if the directory already exists, and to make subdirectories all at the same time
+# Usage(){
+#     echo "Usage: $0 -p <path_to_halper_script> -b <input_bed_file> -o <output_directory> -s <source_species> -t <target_species> -c <alignment_file>" 
+#     exit 1
+# }
+
+# example runs: use optimal peaks for all but the human ovary
+# mouse pancreas:
+# sbatch submit_hal.sh -b "/ocean/projects/bio230007p/ikaplow/MouseAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz" -o ~/output/hal/Mouse/Pancreas -s Mouse -t Human
+# mouse ovary:
+# sbatch submit_hal.sh -b "/ocean/projects/bio230007p/ikaplow/MouseAtac/Ovary/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz" -o ~/output/hal/Mouse/Ovary -s Mouse -t Human
+# human pancreas:
+# sbatch submit_hal.sh -b "/ocean/projects/bio230007p/ikaplow/HumanAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz" -o ~/output/hal/Human/Pancreas -s Human -t Mouse
+# human ovary:
+# sbatch submit_hal.sh -b "/ocean/projects/bio230007p/ikaplow/HumanAtac/Ovary/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz" -o ~/output/hal/Human/Ovary -s Human -t Mouse
+
+help_str="
+Parameters:
+  -h, --help:     Print out the manual
+  -d, --dir:      halLiftover-postprocessing directory    Optional parameter
+  -b, --bed:      Input BED or narrowpeak file            Mandatory parameter
+  -o, --output:   Output directory                        Optional parameter
+  -s, --source:   Source species                          Mandatory parameter
+  -t, --target:   Target species                          Mandatory parameter
+  -c, --align:    Alignment File                          Optional parameter
+"
+
+ARGS=`getopt -o d:b:o:s:t:c:h --long dir:,bed:,output:,source:,target:,align:,help -- "$@"`
+if [ $? != 0 ]; then
+   echo "Error parsing arguments"
+   exit 1
+fi
+
+eval set -- "${ARGS}"
+
+while true
+do
+  case "$1" in
+
+    -h|--help)
+      echo -e "$help_str"
+      exit
+      ;;
+
+    -d|--dir)
+      dir="$2"
+      shift 2
+      ;;
+
+    -b|--bed)
+      bed="$2"
+      shift 2
+      ;;
+
+    -o|--output)
+      out="$2"
+      shift 2
+      ;;
+
+    -s|--source)
+      source="$2"
+      shift 2
+      ;;
+
+    -t|--target)
+      target="$2"
+      shift 2
+      ;;
+
+    -c|--align)
+      align="$2"
+      shift 2
+      ;;
+
+    --)
+      shift
+      break
+      ;;
+
+    *)
+      echo "Internal error: unknown flags"
+      exit 1
+      ;;
+  esac
+done
+
+# assign default values if needed
+DEFAULT_DIR="$(pwd)/halLiftover-postprocessing"
+DEFAULT_OUTPUT="$(pwd)/output"
+DEFAULT_ALIGN="/ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal"
+
+[ -z "$dir" ] && dir="$DEFAULT_DIR"
+[ -z "$out" ] && out="$DEFAULT_OUTPUT"
+[ -z "$align" ] && align="$DEFAULT_ALIGN"
 
 
-# use optimal peaks for all but the human ovary
-peak="/ocean/projects/bio230007p/ikaplow/MouseAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz"
-# peak="/ocean/projects/bio230007p/ikaplow/MouseAtac/Ovary/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz"
-# peak="/ocean/projects/bio230007p/ikaplow/HumanAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz"
-# peak="/ocean/projects/bio230007p/ikaplow/HumanAtac/Ovary/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz"
-# this one was the data I previously used as the conservative peaks for human ovary
-# peak="/ocean/projects/bio230007p/ikaplow/HumanAtac/Ovary/peak/rep1_vs_rep2/rep1_vs_rep2.idr0.05.bfilt.narrowPeak.gz"
-# peak could be a CLI, but I'm hard-coding it for now
+# check output folder
+if [ ! -d "$out" ]; then
+    mkdir -p "$out" # -p flag to create all subdirectories at the same time if $out is a set of nested directories
+    echo "Created output folder: $out"
+fi
 
-mkdir -p input # make an input directory for convenience
-yes | cp $peak ~/input/peaks.narrowPeak.gz # for convenience, copy the peaks file into the input directory under a known name
-gunzip -f ~/input/peaks.narrowPeak.gz # The halLiftover documentation claimed that .gz files were okay, but you do need to unzip
-# use yes command or -f to overwrite files if they already exist
+# check mandatory inputs
+[ -z "$bed" ] && echo "No Input BED or narrowpeak file! Terminating..." && exit 1
+[ -z "$source" ] && echo "No Input Source! Terminating..." && exit 1
+[ -z "$target" ] && echo "No Input Target! Terminating..." && exit 1
 
-source="Mouse"
-# source="Human"
+exe="$dir/halper_map_peak_orthologs.sh"
 
-target="Human"
-# target="Mouse"
+# check the files
+[ -f "$exe" ] || { echo "Cannot Locate halper_map_peak_orthologs! Terminating..."; exit 1; }
+[ -x "$exe" ] || { echo "halper_map_peak_orthologs Not Executable! Terminating..."; exit 1; }
+[ -f "$bed" ] || { echo "Cannot Locate BED or Narrowpeak File! Terminating..."; exit 1; }
+[ -f "$align" ] || { echo "Cannot Locate Alignment File! Terminating..."; exit 1; }
 
-# Line by line breakdown of commands:
-# 1: Script reference is an absolute path from the home directory to ensure we can find it
-# It assumes that you've cloned the halper repository to a subdirectory called repos.
-# 2: input peak file as .narrowPeak or .bed; should make into a CLI. Supposedly halLiftover will work on .gz files, but if not, then extract it before running 
-# 3: out directory, already set up as a variable. Later, parse it from CLI.
-# 4: source species, i.e. the species the .narrowPeak or .bed file came from. Should make into a CLI. Only accept Human or Mouse; all others should give an error.
-# 5: target species. Could use a conditional: if source species is Human, pick Mouse, and vice versa.
-# 6: path to Cactus alignment file; make into a CLI.
+# if input $bed is a .gz file, unzip it
+if [[ "$bed" == *.gz ]]; then
+  # Create ~/input directory if it doesn't exist
+  # We unzip to the ~/input directory in case the original bed file's directory is read-only.
+  mkdir -p ~/input
 
-~/repos/halLiftover-postprocessing/halper_map_peak_orthologs.sh \
-	-b ~/input/peaks.narrowPeak \
-	-o $outdir \
-	-s $source \
-	-t $target \
-	-c /ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal
+  # Unzip while keeping original file (-k) and force overwrite (-f)
+  # Directly output to destination
+  gunzip -k -f -c "$bed" > ~/input/peaks.narrowPeak # I tried preserving the source file's name, but it was causing issues
+
+  # Update the bed variable
+  bed=~/input/peaks.narrowPeak
+
+  echo "Extracted the compressed BED to $bed"
+fi
+
+# finally, run the halper script
+"$exe" -b "$bed" -o "$out" -s "$source" -t "$target" -c "$align"
 
 echo "HAL analysis complete!"
