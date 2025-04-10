1a12e9f714c2b7c86facec793324094c68f05a39
Aayushi Soni <aayushijsoni@gmail.com>
2025-04-10 19:06:59 -0400

    Update intraspecies_open_chromatin_analysis.sh

--- intraspecies_open_chromatin_analysis.sh
+++ intraspecies_open_chromatin_analysis.sh
@@ -8,38 +8,39 @@ module load bedtools
 Project_directory="/ocean/projects/bio230007p/asoni2"
 Output_directory="${Project_directory}/open_chromatin_analysis"
 
-# Create output folders
+# Create subdirectiories to save human and mouse outputs
 mkdir -p "${Output_directory}/human"
 mkdir -p "${Output_directory}/mouse"
 
-#Unzip peak files
-gunzip -c /ocean/projects/bio230007p/ikaplow/HumanAtac/Ovary/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz > human_ovary.bed
-gunzip -c /ocean/projects/bio230007p/ikaplow/HumanAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz > human_pancreas.bed
-gunzip -c /ocean/projects/bio230007p/ikaplow/MouseAtac/Ovary/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz > mouse_ovary.bed
-gunzip -c /ocean/projects/bio230007p/ikaplow/MouseAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz > mouse_pancreas.bed
+#Unzip peak files and sort by genomic coordinate
+gunzip -c /ocean/projects/bio230007p/ikaplow/HumanAtac/Ovary/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz | sort -k1,1 -k2,2n > human_ovary.bed
+gunzip -c /ocean/projects/bio230007p/ikaplow/HumanAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz | sort -k1,1 -k2,2n > human_pancreas.bed
+gunzip -c /ocean/projects/bio230007p/ikaplow/MouseAtac/Ovary/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz | sort -k1,1 -k2,2n > mouse_ovary.bed
+gunzip -c /ocean/projects/bio230007p/ikaplow/MouseAtac/Pancreas/peak/idr_reproducibility/idr.optimal_peak.narrowPeak.gz | sort -k1,1 -k2,2n > mouse_pancreas.bed
+
 
 # Human - Ovary first
-# Shared regions open in both tissues
-bedtools intersect -a human_ovary.bed -b human_pancreas.bed > "${Output_directory}/human/shared_regions_ovaryfirst.bed"
-# Regions open in ovary only
+#Output BED file: regions in ovary that also overlap pancreas
+bedtools intersect -a human_ovary.bed -b human_pancreas.bed -u > "${Output_directory}/human/shared_regions_ovaryfirst.bed"
+#Output BED file: open regions unique to ovary
 bedtools intersect -a human_ovary.bed -b human_pancreas.bed -v > "${Output_directory}/human/ovaryonly_regions_ovaryfirst.bed"
 
 # Human - Pancreas first
-# Shared regions open in both tissues
-bedtools intersect -a human_pancreas.bed -b human_ovary.bed > "${Output_directory}/human/shared_regions_pancreasfirst.bed"
-# Regions open in pancreas only
+#Output BED file: regions in pancreas that also overlap ovary
+bedtools intersect -a human_pancreas.bed -b human_ovary.bed -u > "${Output_directory}/human/shared_regions_pancreasfirst.bed"
+#Output BED file: open regions unique to pancreas
 bedtools intersect -a human_pancreas.bed -b human_ovary.bed -v > "${Output_directory}/human/pancreas_regions_only.bed"
 
 # Mouse - Ovary first
-# Shared regions open in both tissues
-bedtools intersect -a mouse_ovary.bed -b mouse_pancreas.bed > "${Output_directory}/mouse/shared_regions_ovaryfirst.bed"
-# Regions open in ovary only
+#Output BED file: regions in ovary that also overlap pancreas
+bedtools intersect -a mouse_ovary.bed -b mouse_pancreas.bed -u > "${Output_directory}/mouse/shared_regions_ovaryfirst.bed"
+#Output BED file: open regions unique to ovary
 bedtools intersect -a mouse_ovary.bed -b mouse_pancreas.bed -v > "${Output_directory}/mouse/ovaryonly_regions_ovaryfirst.bed"
 
 # Mouse - Pancreas first
-# Shared regions open in both tissues
+#Output BED file: regions in pancreas that also overlap ovary
 bedtools intersect -a mouse_pancreas.bed -b mouse_ovary.bed > "${Output_directory}/mouse/shared_regions_pancreasfirst.bed"
-# Regions open in pancreas only
+#Output BED file: open regions unique to pancreas
 bedtools intersect -a mouse_pancreas.bed -b mouse_ovary.bed -v > "${Output_directory}/mouse/pancreas_regions_only.bed"
 
 
