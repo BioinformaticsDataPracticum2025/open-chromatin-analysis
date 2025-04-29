401e93eed3ea2ec0a6309aad37a49cd382b280e7
Katherine Wang <kwang18@br014.ib.bridges2.psc.edu>
2025-04-28 23:54:18 -0400

    Connected part of step 6 to pipeline

--- main.sh
+++ main.sh
@@ -149,10 +149,16 @@ step5_outdir="${pipe_out}/enhancers_and_promoters"
 mkdir -p $step5_outdir
 
 
-# TODO: get step 6 inputs.
-# TODO: should ask the user for filenames of the .meme databases, one for each species. Also add this to GitHub example inputs.
+# Get step 6 inputs.
 echo "Taking inputs for motif analysis (step 6)"
-# TODO read -p 
+
+# Get reference genomes for species 1 and species 2
+read -p "Enter a reference genome for ${species1}, with a file ending in either .fasta or .fa: " s1_genome
+read -p "Enter a reference genome for ${species2}, with a file ending in either .fasta or .fa: " s2_genome
+
+# Get filenames of the .meme databases, one for each species.
+read -p "Enter a .meme motif database file for ${species1}: " s1_meme
+read -p "Enter a .meme motif database file for ${species2}: " s2_meme
 
 # Also set up a directory for step 6 outputs, which will be passed to MEME-ChIP
 step6_outdir="${pipe_out}/motifs"
@@ -267,7 +273,7 @@ python python_scripts/plot_radial_new.py --names "$names_string" --jaccard "$jac
 
 echo "Step 4 of the pipeline is to upload the steps 2a and 3 outputs to the GREAT webtool here: http://great.stanford.edu/public/html/"
 
-# TODO: run step 5
+# Run step 5
 # Some bedtools intersections are done to produce intermediate files, and we don't need the Jaccard for them. In that case, use bedtools intersect directly.
 # For the ones that require Jaccard, use run_bedtools.
 module load bedtools
@@ -329,7 +335,7 @@ bedtools intersect -a $s1t1_hal -b $s2p -u > $s1t1p_hal
 bedtools intersect -a $s1t2_hal -b $s2p -u > $s1t2p_hal
 
 
-# use run_bedtools for this set of intersections because we want to track the Jaccard for these intersections
+# use run_bedtools for this set of intersections (step 5a) because we want to track the Jaccard for these intersections
 echo "Running step 5a, which compares the Jaccard index of enhancers shared across tissues to the Jaccard index of promoters shared across tissues."
 # reset jaccard_arr and names_arr because we'll make a new plot for this set of intersections
 jaccard_arr=()
@@ -338,74 +344,148 @@ names_arr=()
 mkdir -p "${step5_outdir}/6d"
 echo "Finding enhancers shared between ${tissue1} and ${tissue2} within each species... Outputs will go in ${step5_outdir}/6d"
 # For this, we need to intersect the files that we just created in the 6b directory.
-run_bedtools "$s1t1e" "$s1t2e" "${step5_outdir}/6d/${species1}_crossTissue_enhancers.bed" "y" "${species1} enhancers shared across tissues"
-run_bedtools "$s2t1e" "$s2t2e" "${step5_outdir}/6d/${species2}_crossTissue_enhancers.bed" "y" "${species2} enhancers shared across tissues"
+s1e_shared="${step5_outdir}/6d/${species1}_crossTissue_enhancers.bed"
+s2e_shared="${step5_outdir}/6d/${species2}_crossTissue_enhancers.bed"
+run_bedtools "$s1t1e" "$s1t2e" "$s1e_shared" "y" "${species1} enhancers shared across tissues"
+run_bedtools "$s2t1e" "$s2t2e" "$s2e_shared" "y" "${species2} enhancers shared across tissues"
 
 echo "Finding promoters shared between ${tissue1} and ${tissue2} within each species... Outputs will go in ${step5_outdir}/6d"
 # For this, we need to intersect the files that we just created in the 6c directory.
 # Note that these promoter intersection BED files aren't actually used in step 6d, or in step 6 at all.
-run_bedtools "$s1t1p" "$s1t2p" "${step5_outdir}/6d/${species1}_crossTissue_promoters.bed" "y" "${species1} promoters shared across tissues"
-run_bedtools "$s2t1p" "$s2t2p" "${step5_outdir}/6d/${species2}_crossTissue_promoters.bed" "y" "${species2} promoters shared across tissues"
+s1p_shared="${step5_outdir}/6d/${species1}_crossTissue_promoters.bed"
+s2p_shared="${step5_outdir}/6d/${species2}_crossTissue_promoters.bed"
+run_bedtools "$s1t1p" "$s1t2p" "$s1p_shared" "y" "${species1} promoters shared across tissues"
+run_bedtools "$s2t1p" "$s2t2p" "$s2p_shared" "y" "${species2} promoters shared across tissues"
 
 # visualize the Jaccard for cross-tissue comparisons
 echo "Check ${pipe_out} for visualizations of the step 5a (cross-tissue comparison) Jaccard indices!"
 jaccard_string=$(IFS=,; echo "${jaccard_arr[*]}")
 names_string=$(IFS=,; echo "${names_arr[*]}")
-python python_scripts/plot_radial_new.py --names "$names_string" --jaccard "$jaccard_string" --out "${pipe_out}/step5a_jaccard.png"
+python python_scripts/plot_radial_new.py --names "$names_string" --jaccard "$jaccard_string" --out "${pipe_out}/step5a_cross_tissue_jaccard.png"
 
 echo "Step 5a has been completed."
+
 
 # 6e has nothing to do with either step 5a or step 5b
 mkdir -p "${step5_outdir}/6e"
 echo "Finding enhancers specific to ${tissue1} and specific to ${tissue2} within each species... Outputs will go in ${step5_outdir}/6e"
 # This is like the first part of the 6d set of intersections, but ultimately with a -v flag instead of -u flag (as -u underlies the "y" mode)
-# TODO
+
+# For species 1:
+s1e_t1only="${step5_outdir}/6e/${species1}_${tissue1}Only_enhancers.bed"
+bedtools intersect -a $s1t1e -b $s1t2e -v > $s1e_t1only
+s1e_t2only="${step5_outdir}/6e/${species1}_${tissue2}Only_enhancers.bed"
+bedtools intersect -a $s1t2e -b $s1t1e -v > $s1e_t2only
+
+# For species 2:
+s2e_t1only="${step5_outdir}/6e/${species2}_${tissue1}Only_enhancers.bed"
+bedtools intersect -a $s2t1e -b $s2t2e -v > $s2e_t1only
+s2e_t2only="${step5_outdir}/6e/${species2}_${tissue2}Only_enhancers.bed"
+bedtools intersect -a $s2t2e -b $s2t1e -v > $s2e_t2only
 
 
 
-
-# use run_bedtools for this set of intersections as well
+# use run_bedtools for this set of intersections (step 5b) because we want to track the Jaccard for these intersections
+mkdir -p "${step5_outdir}/6f"
 echo "Running step 5b, which compares the Jaccard index of enhancers shared across species to the Jaccard index of promoters shared across species."
-echo "Finding enhancers shared across species for each tissue..."
 # reset jaccard_arr and names_arr because we'll make a new plot for this set of intersections
 jaccard_arr=()
 names_arr=()
 
+echo "Finding enhancers shared across species for each tissue... Outputs will go in ${step5_outdir}/6f"
+t1e_shared="${step5_outdir}/6f/bothSpecies_${tissue1}_enhancers.bed"
+t2e_shared="${step5_outdir}/6f/bothSpecies_${tissue2}_enhancers.bed"
+run_bedtools "$s1t1e_hal" "$s2t1e" "$t1e_shared" "y" "${tissue1} enhancers shared across species"
+run_bedtools "$s1t2e_hal" "$s2t2e" "$t2e_shared" "y" "${tissue2} enhancers shared across species"
 
-
-
-
-
-
-mkdir -p "${step5_outdir}/6f"
 echo "Finding promoters shared between ${species1} and ${species2} for each tissue... Outputs will go in ${step5_outdir}/6f"
+t1p_shared="${step5_outdir}/6f/bothSpecies_${tissue1}_promoters.bed"
+t2p_shared="${step5_outdir}/6f/bothSpecies_${tissue2}_promoters.bed"
+run_bedtools "$s1t1p_hal" "$s2t1p" "$t1p_shared" "y" "${tissue1} promoters shared across species"
+run_bedtools "$s1t2p_hal" "$s2t2p" "$t2p_shared" "y" "${tissue2} promoters shared across species"
 
-
-# also do the Jaccard stuff.
-# echo "Check ${pipe_out} for visualizations of the step 5 Jaccard index!"
-# jaccard_string=$(IFS=,; echo "${jaccard_arr[*]}")
-# names_string=$(IFS=,; echo "${names_arr[*]}")
-# python python_scripts/plot_radial_new.py --names "$names_string" --jaccard "$jaccard_string" --out "${pipe_out}/step5_jaccard.png"
+# visualize the Jaccard.
+echo "Check ${pipe_out} for visualizations of the step 5b (cross-species comparison) Jaccard indices!"
+jaccard_string=$(IFS=,; echo "${jaccard_arr[*]}")
+names_string=$(IFS=,; echo "${names_arr[*]}")
+python python_scripts/plot_radial_new.py --names "$names_string" --jaccard "$jaccard_string" --out "${pipe_out}/step5b_cross_species_jaccard.png"
 
 echo "Step 5b has been completed."
 
 
-echo "Finding enhancers specific to each species for each tissue..."
-# TODO: make a subdirectory, and echo it (6g)
+mkdir -p "${step5_outdir}/6g"
+echo "Finding enhancers specific to each species for each tissue... Outputs will go in ${step5_outdir}/6g"
+
+# For tissue 1:
+t1e_s1only="${step5_outdir}/6g/${species1}Only_${tissue1}_enhancers.bed"
+t1e_s2only="${step5_outdir}/6g/${species2}Only_${tissue1}_enhancers.bed"
+bedtools intersect -a $s1t1e_hal -b $s2t1e -v > $t1e_s1only
+bedtools intersect -a $s2t1e -b $s1t1e_hal -v > $t1e_s2only
+
+# For tissue 2:
+t2e_s1only="${step5_outdir}/6g/${species1}Only_${tissue2}_enhancers.bed"
+t2e_s2only="${step5_outdir}/6g/${species2}Only_${tissue2}_enhancers.bed"
+bedtools intersect -a $s1t2e_hal -b $s2t2e -v > $t2e_s1only
+bedtools intersect -a $s2t2e -b $s1t2e_hal -v > $t2e_s2only
+
+echo "Step 5 has been completed!"
 
 
 
-
-# TODO: run step 6 (use sbatch because it'll take a long time to run)
-# echo a message, and make a directory. In this case, you'll pass that directory to MEME (through motif_analysis.sh).
-# there will also be a lot of convert_bed_to_fasta.sh usage
-
-echo "Running step 6a..."
+# Run step 6.
 # for each input file, convert the bed to fasta (bash, not sbatch)
-# then run motif_analysis.sh on it
+# then run motif_analysis.sh on it (which does use sbatch)
+# Make subdirectories for each step within step 6.
+
+mkdir -p "${step6_outdir}/6a"
+echo "Running step 6a... Outputs will go in ${step6_outdir}/6a"
 # Step 6a inputs are simply s1t1, s1t2, s2t1, s2t2
+echo "Converting BED files to FASTA..."
+# TODO: uncomment these 8 lines later
+# bash convert_bed_to_fasta.sh $s1_genome "${s1t1}" "${step6_outdir}/6a/${species1}_${tissue1}.fasta"
+# bash convert_bed_to_fasta.sh $s1_genome "${s1t2}" "${step6_outdir}/6a/${species1}_${tissue2}.fasta"
+# bash convert_bed_to_fasta.sh $s2_genome "${s2t1}" "${step6_outdir}/6a/${species2}_${tissue1}.fasta"
+# bash convert_bed_to_fasta.sh $s2_genome "${s2t2}" "${step6_outdir}/6a/${species2}_${tissue2}.fasta"
+
+job_ids=()
+# job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6a/${species1}_${tissue1}.fasta" "${step6_outdir}/6a/${species1}_${tissue1}" $s1_meme | awk '{print $4}'))
+# job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6a/${species1}_${tissue2}.fasta" "${step6_outdir}/6a/${species1}_${tissue2}" $s1_meme | awk '{print $4}'))
+# job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6a/${species2}_${tissue1}.fasta" "${step6_outdir}/6a/${species2}_${tissue1}" $s2_meme | awk '{print $4}'))
+# job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6a/${species2}_${tissue2}.fasta" "${step6_outdir}/6a/${species2}_${tissue2}" $s2_meme | awk '{print $4}'))
+
+echo "Submitted step 6a jobs with IDs: ${job_ids[*]}"
 
 
-echo "Step 6 has been submitted as a set of slurm jobs."
+mkdir -p "${step6_outdir}/6b"
+echo "Running step 6b... Outputs will go in ${step6_outdir}/6b"
+echo "Converting BED files to FASTA..."
+# TODO: uncomment these 8 lines later
+# bash convert_bed_to_fasta.sh $s1_genome "${s1t1e}" "${step6_outdir}/6b/${species1}_${tissue1}_enhancers.fasta"
+# bash convert_bed_to_fasta.sh $s1_genome "${s1t2e}" "${step6_outdir}/6b/${species1}_${tissue2}_enhancers.fasta"
+# bash convert_bed_to_fasta.sh $s2_genome "${s2t1e}" "${step6_outdir}/6b/${species2}_${tissue1}_enhancers.fasta"
+# bash convert_bed_to_fasta.sh $s2_genome "${s2t2e}" "${step6_outdir}/6b/${species2}_${tissue2}_enhancers.fasta"
+
+job_ids=()
+
+job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6b/${species1}_${tissue1}_enhancers.fasta" "${step6_outdir}/6b/${species1}_${tissue1}_enhancers" $s1_meme | awk '{print $4}'))
+job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6b/${species1}_${tissue2}_enhancers.fasta" "${step6_outdir}/6b/${species1}_${tissue2}_enhancers" $s1_meme | awk '{print $4}'))
+job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6b/${species2}_${tissue1}_enhancers.fasta" "${step6_outdir}/6b/${species2}_${tissue1}_enhancers" $s2_meme | awk '{print $4}'))
+job_ids+=($(sbatch motif_analysis.sh "${step6_outdir}/6b/${species2}_${tissue2}_enhancers.fasta" "${step6_outdir}/6b/${species2}_${tissue2}_enhancers" $s2_meme | awk '{print $4}'))
+
+echo "Submitted step 6b jobs with IDs: ${job_ids[*]}"
+
+
+# TODO: 6c (mkdir and echo it)
+
+# TODO: 6d (mkdir and echo it)
+
+# TODO: 6e (mkdir and echo it)
+
+# TODO: 6f (mkdir and echo it)
+
+# TODO: 6g (mkdir and echo it)
+
+
+echo "All parts of step 6 have been submitted."
 echo "You can check on the progress of step 6 by viewing the slurm-<job_id>.out file. If there are any errors, e.g. in file conversion, they will print to that file."
 echo "Once these sbatch jobs have finished, the pipeline is complete!"
